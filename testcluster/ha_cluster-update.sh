195e9e19bffdc8eef08b92f86f8cec0adcfd6ab5
Thatcher Peskens <thatcher@leaf.cloud>
2022-04-12 12:06:48 +0200

    used narmal and ha

--- testcluster/ha_cluster-update.sh
+++ testcluster/ha_cluster-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COMMAND="openstack coe cluster resize ha-cluster-5 3
+COMMAND="openstack coe cluster upgrade ha-cluster-7 k8s-normal-v1.21.2-template-v2.0-ha-rc2"
 
 echo Executing
 $COMMAND
--- testcluster/ha_cluster.sh
+++ testcluster/ha_cluster.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-COMMAND="openstack coe cluster create ha-cluster-6 \
-    --cluster-template k8s-normal-v1.21.2-template-v2.0-ha-beta6 \
+COMMAND="openstack coe cluster create ha-cluster-8 \
+    --cluster-template k8s-normal-v1.21.2-template-v2.0-ha-rc2 \
     --keypair thatcher_oatcake \
-    --labels auto_scaling_enabled=True,min_node_count=2,max_node_count=10 \
+    --labels auto_scaling_enabled=True,min_node_count=2,max_node_count=3 \
     --merge-labels \
     --node-count 2 \
     --master-count 3 \
-    --master-flavor en1.small \
+    --master-flavor cn1.small \
     --flavor en1.medium \
     --docker-volume-size 12
     " 
